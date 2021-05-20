f95e520da8375780aaa2fe48f5a72afefc74ca82
Vivek Prasad <vivekpsd01@gmail.com>
2021-05-20 11:58:11 +0530

    [Quote-Generator] fetching random quotes from api

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,4 +1,24 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuote array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
 // Get Quotes from API
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch('https://type.fit/api/quotes');
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (err) {
+    //Catch Error Here
+    console.log(err.message);
+  }
 }
+
+//On Load
+getQuotes();
