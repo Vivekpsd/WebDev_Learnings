35a2f59728cd9ee41548962059c5052d37f9beb9
Vivek Prasad <vivekpsd01@gmail.com>
2021-05-20 12:21:48 +0530

    [Quote-Generator] Appending quotes to html through DOM Manipulation

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuote array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  //Appending quotes to html
+  quoteText.innerText = quote.text;
+
+  //Check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  // Check if author field is blank and replace with unknown
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else authorText.innerText = quote.author;
 }
 
 // Get Quotes from API
@@ -19,6 +39,18 @@ async function getQuotes() {
     console.log(err.message);
   }
 }
+
+// To Tweet a quote
+function tweetQuote() {
+  twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+
+twitterBtn.addEventListener('click', tweetQuote);
 
 //On Load
 getQuotes();
