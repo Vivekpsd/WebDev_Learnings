30ab516366abcbc0a45eb3636794f419d30756fb
Vivek Prasad <vivekpsd01@gmail.com>
2021-05-22 11:52:59 +0530

    [Picture-in-Picture] Picture In Picture Functionality Added

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -6,10 +6,25 @@ const button = document.getElementById('button');
 async function selectMediaStream() {
   try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
   } catch (err) {
     console.log(err);
   }
 }
+
+button.addEventListener('click', async () => {
+  // Disable the button
+  button.disabled = true;
+
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset the button
+  button, (disabled = false);
+});
 
 // On load
 selectMediaStream();
